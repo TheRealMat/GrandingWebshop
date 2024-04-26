927901706be376a73abc59ba4ba46f8594a2e996
Mat <mat555333@gmail.com>
2024-04-26 12:13:51 +0200

    how do i set up the scrunglartiy

--- functions/FetchData.js
+++ functions/FetchData.js
@@ -2,6 +2,8 @@
   // Create a prepared statement with our query
   const preparedStatement = context.env.WebshopDB.prepare('SELECT * from Product');
   const data = await preparedStatement.raw();
-
+  console.log(context);
+  console.log(context.env);
+  console.log(context.env.WebshopDB);
   return Response.json(data);
 }
