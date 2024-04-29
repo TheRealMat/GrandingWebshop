0584d5b071125dd5b07b73b6189906df281d884a
Mat <mat555333@gmail.com>
2024-04-29 13:05:30 +0200

    Oops

--- functions/GetProducts.js
+++ functions/GetProducts.js
@@ -1,8 +1,7 @@
-export async function onRequestGet(context) {
-    console.log("Debug 4");
+export async function onRequest(context) {
     //const preparedStatement = context.env.WebshopDB.prepare('SELECT * FROM Product LIMIT 10 OFFSET 0');
     const preparedStatement = context.env.WebshopDB.prepare('SELECT * FROM Product');
     const data = await preparedStatement.all();
-    console.log("Debug 5");
-    return data;
+
+    return Response.json(data);
 }
