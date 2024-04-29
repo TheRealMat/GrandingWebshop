939cb743dea0255e6f1aff79ddae663283b9e030
Mat <mat555333@gmail.com>
2024-04-29 13:47:42 +0200

    Case sensitive?

--- functions/GetProducts.js
+++ functions/GetProducts.js
@@ -1,9 +1,7 @@
 export async function onRequest(context) {
-    console.log("hello")
     //const preparedStatement = context.env.WebshopDB.prepare('SELECT * FROM Product LIMIT 10 OFFSET 0');
     const preparedStatement = context.env.WebshopDB.prepare('SELECT * FROM Product');
     const data = await preparedStatement.all();
-    console.log(context);
     console.log(data);
     return Response.json(data);
 }
