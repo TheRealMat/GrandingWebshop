f75993a76574cd30cdc7f610486c0f627fdfab3e
Mat <mat555333@gmail.com>
2024-04-29 12:24:31 +0200

    The big test

---
+++ functions/GetProducts.js
@@ -0,0 +1,8 @@
+export async function onRequest(context) {
+    console.log("Debug 4");
+    //const preparedStatement = context.env.WebshopDB.prepare('SELECT * FROM Product LIMIT 10 OFFSET 0');
+    const preparedStatement = context.env.WebshopDB.prepare('SELECT * FROM Product');
+    const data = await preparedStatement.all();
+    console.log("Debug 5");
+    return data;
+}
