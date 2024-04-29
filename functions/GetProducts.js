19ff8f4d81444586f096af59129431adc83bb006
Mat <mat555333@gmail.com>
2024-04-29 12:43:49 +0200

    Fix?

--- functions/GetProducts.js
+++ functions/GetProducts.js
@@ -1,4 +1,4 @@
-export async function onRequest(context) {
+export async function onRequestGet(context) {
     console.log("Debug 4");
     //const preparedStatement = context.env.WebshopDB.prepare('SELECT * FROM Product LIMIT 10 OFFSET 0');
     const preparedStatement = context.env.WebshopDB.prepare('SELECT * FROM Product');
