6b71867ba6c85b91b1af30c4a12cc9199c67aaf8
Mat <mat555333@gmail.com>
2024-05-01 12:45:22 +0200

    Test get from category

--- functions/get_products.js
+++ functions/get_products.js
@@ -1,5 +1,7 @@
 export async function onRequest(context) {
-    const preparedStatement = context.env.WebshopDB.prepare('SELECT * FROM Product LIMIT 10 OFFSET 0');
+    debug.log(context);
+    debug.log(context.request);
+    const preparedStatement = context.env.WebshopDB.prepare('SELECT * FROM ProductCategories INNER JOIN Product ON Product.Id = ProductCategories.Product_Id WHERE Category_Id = ? LIMIT 10 OFFSET ?').bind(context.request,0);
     const data = await preparedStatement.all();
     return Response.json(data);
 }
