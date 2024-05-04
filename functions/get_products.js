d53d90eb1c03977aaddf5ace7ab0bb382b0b27d2
Mat <mat555333@gmail.com>
2024-05-04 15:18:03 +0200

    deug to console

--- functions/get_products.js
+++ functions/get_products.js
@@ -1,6 +1,6 @@
 export async function onRequest(context) {
-    debug.log(context);
-    debug.log(context.request);
+    console.log(context);
+    console.log(context.request);
     const preparedStatement = context.env.WebshopDB.prepare('SELECT * FROM ProductCategories INNER JOIN Product ON Product.Id = ProductCategories.Product_Id WHERE Category_Id = ? LIMIT 10 OFFSET ?').bind(context.request,0);
     const data = await preparedStatement.all();
     return Response.json(data);
