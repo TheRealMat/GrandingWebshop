be6bf859d31f81f83cceaf7ce39d4d721e00b4a0
Mat <mat555333@gmail.com>
2024-05-04 22:41:41 +0200

    Moved models

--- Models/ProductModel.cs
+++ Models/ProductModel.cs
@@ -16,4 +16,23 @@
 
         public string? ImageUrl { get; set; }
     }
+
+	public class Meta
+	{
+		public string? served_by { get; set; }
+		public double duration { get; set; }
+		public int changes { get; set; }
+		public int last_row_id { get; set; }
+		public bool changed_db { get; set; }
+		public int size_after { get; set; }
+		public int rows_read { get; set; }
+		public int rows_written { get; set; }
+	}
+
+	public class Root
+	{
+		public bool success { get; set; }
+		public Meta? meta { get; set; }
+		public ProductModel[]? results { get; set; }
+	}
 }
