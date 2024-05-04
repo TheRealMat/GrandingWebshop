78d08f41e2360cfd492a479c72d294a9ed56363e
Mat <mat555333@gmail.com>
2024-05-04 17:16:32 +0200

    Oops

--- functions/get_products.js
+++ functions/get_products.js
@@ -3,7 +3,7 @@ export async function onRequest(context) {
     console.log(context);
     console.log("context.request: ");
     console.log(context.request);
-    const preparedStatement = context.env.WebshopDB.prepare('SELECT * FROM Product WHERE ProductCategories.ProductId = ?1').bind(1);
+    const preparedStatement = context.env.WebshopDB.prepare('SELECT * FROM ProductCategories INNER JOIN Product ON Product.Id = ProductCategories.ProductId WHERE CategoryId = ?1').bind(1);
     const data = await preparedStatement.all();
     return Response.json(data);
 }
