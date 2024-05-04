48315b80c49fd78942061951117fd8a71ca3ef15
Mat <mat555333@gmail.com>
2024-05-05 00:26:48 +0200

    Routing based category queries

---
+++ Models/Enums.cs
@@ -0,0 +1,31 @@
+﻿namespace GrandingWebshop.Models
+{
+	// No, these aren't really models
+	public class Enums
+	{
+		// These may need to manually be assigned values eventually
+		public enum categories
+		{
+			Kunst,
+			Collager,
+			Malerier,
+			Papirklip,
+			Fotokunst,
+			Print,
+			Accessories,
+			Tasker,
+			Tørklæder,
+			Nøgleringe,
+			Smykker,
+			Armbånd,
+			Øreringe,
+			Interiør,
+			Opslagstavler,
+			Papirlygter,
+			Årstidsbestemt,
+			Jul,
+			Påske,
+			Halloween
+		}
+	}
+}
