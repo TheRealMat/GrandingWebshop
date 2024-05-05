bf7fe7ea415fe1263901c713df96f039388877f2
Mat <mat555333@gmail.com>
2024-05-05 21:28:46 +0200

    Cart visuals

---
+++ Cart.cs
@@ -0,0 +1,16 @@
+﻿using GrandingWebshop.Models;
+using System.Net.Http.Json;
+
+namespace GrandingWebshop
+{
+    public class Cart
+    {
+
+        public List<ProductModel> products { get; set; } = new();
+
+        public async Task AddProductToCartAsync(ProductModel product)
+        {
+			products.Add(new ProductModel());
+		}
+    }
+}
